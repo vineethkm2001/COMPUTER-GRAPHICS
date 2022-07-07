02a300ff4450a1f28936469a436d6e67fb238008
VINEETH KM <93125729+vineethkm2001@users.noreply.github.com>
2022-07-07 16:26:28 +0530

    Update lab8.c

--- lab8.c
+++ lab8.c
@@ -68,7 +68,7 @@ int main(int argc, char *argv[]){
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
 	glutInitWindowSize(640,480);
 	glutInitWindowPosition(100,100);
-	glutCreateWindow("4MT19CS185 Teapot");
+	glutCreateWindow("4MT19CS177 Teapot");
 	glutDisplayFunc(displaySolid);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
